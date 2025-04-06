f7c96f921392066e047e075ab0ce07b0c12ccb28
Myunghee-Yoon <mhee.yoon@gmail.com>
2025-04-07 02:49:26 +0900

    Add GitHub Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/ClassyRoom/', // GitHub Pages 배포를 위한 base 경로 설정
   plugins: [react()],
 });
